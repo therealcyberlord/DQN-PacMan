b100521c634df225762fdeb1f69725c09c32673a
Xingyu Bian <xingyubiancyberland@gmail.com>
2022-07-16 14:50:53 -0400

    updated logging

--- Logging.py
+++ Logging.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 # help us keep track of the progress during training
 
 class Logger:
-    def __init__(self, num_episodes):
+    def __init__(self, num_episodes, average_reward_period):
         self.num_episodes = num_episodes 
         self.episodes = []
         self.steps = [] 
         self.rewards = []
+        self.average_rewards = [] 
         self.total_steps = [] 
-        self.huber_loss = []
+        self.average_period = average_reward_period
+        self.best_average = 0 
 
     def print_stats(self):
         # this prints out the latest stats
@@ -17,27 +19,34 @@ class Logger:
 
         print(f"Episode {self.episodes[-1]}/{self.num_episodes}")
         print("Rewards:", self.rewards[-1])
-        print("Loss:", self.huber_loss[-1])
+        print("Average Rewards: %.4f" % self.average_rewards[-1])
+        print("Best Average Rewards: %.4f" % self.best_average)
         print("Steps:", self.steps[-1])
         print("Total Steps:", self.total_steps[-1])
         print("--------------------")
         print("")
 
     # record statistics after an episode
-    def record(self, episode_num, episode_rewards, loss, episode_steps, total_steps):
+    def record(self, episode_num, episode_rewards, episode_steps, total_steps):
         self.episodes.append(episode_num)
         self.rewards.append(episode_rewards)
-        self.huber_loss.append(loss.cpu().item())
+        self.average_rewards.append(np.mean(self.rewards[-self.average_period:]))
         self.steps.append(episode_steps)
         self.total_steps.append(total_steps)
 
+    # check if the current average is better than the best average 
+    def new_best_average(self):
+        if self.average_rewards[-1] > self.best_average:
+            self.best_average = self.average_rewards[-1]
+            return True 
+        return False 
+
+
     def plot(self):
-        # plot the reward over the episodes
+        # plot the average reward over the episodes
         plt.figure(figsize=(8, 6))
         plt.title("Rewards over the episodes")
-        average = np.mean(self.rewards)
-        plt.plot(self.rewards)
-        plt.axhline(y = average, color = 'r', linestyle = 'dashed')
+        plt.plot(self.average_rewards)
         plt.xlabel("Episodes")
         plt.ylabel("Scores")
         plt.show()
@@ -45,9 +54,7 @@ class Logger:
         # plot the huber loss over the episodes
         plt.figure(figsize=(8, 6))
         plt.title("Loss over the episodes")
-        average = np.mean(self.huber_loss)
         plt.plot(self.huber_loss)
-        plt.axhline(y = average, color = 'r', linestyle = 'dashed')
         plt.xlabel("Episodes")
         plt.ylabel("Huber Loss")
         plt.show()
